3bd1d7539425e0ddfe3d462406aee76a82d9cb40
amauri <amauri@DESKTOP-98ON28K>
2016-10-15 19:54:38 -0300

    q

--- Governo/src/br/com/governo/business/UsuarioService.java
+++ Governo/src/br/com/governo/business/UsuarioService.java
@@ -14,6 +14,11 @@ public class UsuarioService {
 	
 	static {
 		usuarios.put("12345678901", new Usuario("12345678901", null, "123"));
+		usuarios.put("99334696494", new Usuario("99334696494", null, "123"));
+		usuarios.put("67341083689", new Usuario("67341083689", null, "123"));
+		usuarios.put("22532243000103", new Usuario(null, "22532243000103", "123"));
+		usuarios.put("03169483000140", new Usuario(null, "03169483000140", "123"));
+		usuarios.put("92252235000199", new Usuario(null, "92252235000199", "123"));
 	}
 	
 	public Usuario recuperarUsuario(String cpfOuCnpj) {
